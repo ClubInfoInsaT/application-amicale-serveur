a726aee1314bc3529fc6c658e88e03774a82c67e
Arnaud Vergnet <vergnet@etud.insa-toulouse.fr>
2020-06-24 14:55:05 +0200

    Fixed script path error

--- dashboard/dashboard_update.sh
+++ dashboard/dashboard_update.sh
@@ -3,6 +3,8 @@
 # Update washing machines
 cd "$HOME"/public_html/v2/washinsa && ./washinsa_update.sh
 
+cd "$HOME"/public_html/v2/dashboard || exit
+
 # Update the dashboard with the new machine list
 touch lock
 python3 handler.py > log 2> err
