6c5308ed933cc266b68c423797ad8b4947da3e26
Arnaud Vergnet <vergnet@etud.insa-toulouse.fr>
2020-06-24 18:24:48 +0200

    Changed script names

--- dashboard/dashboard_update.sh
+++ dashboard/dashboard_update.sh
@@ -7,5 +7,5 @@ cd "$HOME"/public_html/v2/dashboard || exit
 
 # Update the dashboard with the new machine list
 touch lock
-python3 handler.py > log 2> err
+python3 dashboard_handler.py > log 2> err
 rm lock
--- menu/menu_update.sh
+++ menu/menu_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch lock
-curl "https://webservices-v2.crous-mobile.fr/ws/v1/regions/5/restaurants/114/menus" > menu_data.json && python3 handler.py > log 2> err
+curl "https://webservices-v2.crous-mobile.fr/ws/v1/regions/5/restaurants/114/menus" > menu_data.json && python3 menu_handler.py > log 2> err
 rm lock
