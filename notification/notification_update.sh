b4f21197ffa3d06dff971c4bb9435fd85d53504e
Paul ALNET <paul@shimaore.net>
2022-11-25 11:33:08 +0100

    notif : correct wget command to bypass proxy and name file

--- notification/notification_update.sh
+++ notification/notification_update.sh
@@ -6,6 +6,8 @@ cd "$HOME"/public_html/v2/notification || exit
 # Note that is a temporary test solution.
 # Notifications should be served from the amicale server
 touch lock
-wget https://amicale-app-dev.insat.fr/notification/list.json
+# --no-proxy to bypass cache.insa-toulouse.fr
+# -O list.json to avoid getting files named list.json.1
+wget --no-proxy -O list.json https://amicale-app-dev.insat.fr/notification/list.json
 rm lock
 
