5aed1eb56042555105b8a499af764250295fd5ae
Paul ALNET <paul@shimaore.net>
2023-08-14 15:31:43 +0200

    fix(washinsa): update origin header

--- washinsa/washinsa_handler.py
+++ washinsa/washinsa_handler.py
@@ -76,7 +76,7 @@ def fetch_data(code: str):
     """
     url = craft_url(code)
     headers = {
-        'Origin': 'https://beta.proxiwash.com',
+        'Origin': 'https://proxiwash.com',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
     }
     try:
