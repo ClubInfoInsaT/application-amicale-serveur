b4690022a44f4a78a0623302e44c10125eee3313
Paul ALNET <paul@shimaore.net>
2023-12-20 17:25:50 +0100

    fix(washinsa): set accept-language header

--- washinsa/washinsa_handler.py
+++ washinsa/washinsa_handler.py
@@ -77,7 +77,8 @@ def fetch_data(code: str):
     url = craft_url(code)
     headers = {
         'Origin': 'https://www.proxiwash.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0',
+        'Accept-Language': 'fr,fr-FR;q=0.8'
     }
     try:
         print('Fetching data from : ' + url)
